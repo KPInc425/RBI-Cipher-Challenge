8ed8e03d472a10b1b77b03e44759364f4e2b218a
KPInc425 <kpinc425@gmail.com>
2024-06-07 14:37:04 -0700

    update

---
+++ netlify/functions/sendMail.mjs
@@ -0,0 +1,30 @@
+const sendMail = async ({name, email, subject, message}) => {
+  const WEB3FORMS_ACCESS_KEY = process.env.WEB3FORMS_ACCESS_KEY;
+  const response = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      access_key: WEB3FORMS_ACCESS_KEY,
+      from_name: "RBI - The Coding Challenge",
+      name: name,
+      subject: subject,
+      email: email,
+      message: message,
+    }),
+  });
+
+  const result = await response.json();
+  if (result.success) {
+    console.log("Email Sent.")
+    console.log(result);
+    return result;
+  } else {
+    console.error(result);
+    return result;
+  }
+}
+
+export default sendMail;
