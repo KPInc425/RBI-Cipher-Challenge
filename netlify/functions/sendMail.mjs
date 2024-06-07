007f68a9c6ffae5f87ddee54a5f5ac8a68a6f08d
KPInc425 <kpinc425@gmail.com>
2024-06-07 14:50:52 -0700

    update access key for sending mail

--- netlify/functions/sendMail.mjs
+++ netlify/functions/sendMail.mjs
@@ -1,7 +1,5 @@
-import {process} from "process";
-
 const sendMail = async ({name, email, subject, message}) => {
-  const WEB3FORMS_ACCESS_KEY = process.env.WEB3FORMS_ACCESS_KEY;
+  const WEB3FORMS_ACCESS_KEY = "15c3b44c-999f-46e8-82d3-f8a9b9e8531d";
   const response = await fetch("https://api.web3forms.com/submit", {
     method: "POST",
     headers: {
