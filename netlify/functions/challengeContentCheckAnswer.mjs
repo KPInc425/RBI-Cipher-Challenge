4a7a8721ac16bbe493300ae7fb0ec9d63e834dc3
JamesK <James@10-geek.com>
2024-06-25 17:47:23 -0500

    casting to url

--- netlify/functions/challengeContentCheckAnswer.mjs
+++ netlify/functions/challengeContentCheckAnswer.mjs
@@ -2,8 +2,7 @@ export default async (event, context) => {
   console.log("event log:");
   console.log(event);
   console.log(event.url);
-  
-  const answer = event.url.searchParams.get("answer");
+  const answer = new URL(event.url).searchParams.get("answer");
   console.log(answer)
   
   return Response.json({ "isCorrect": (String(answer).toLowerCase() == String("Amazing job, very well done, your journey has just begun!").toLowerCase())})
