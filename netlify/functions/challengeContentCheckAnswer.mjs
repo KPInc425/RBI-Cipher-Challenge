295b523a92eacdf20127064d3bded57bd18bfabb
JamesK <James@10-geek.com>
2024-06-25 17:30:36 -0500

    url searchParams

--- netlify/functions/challengeContentCheckAnswer.mjs
+++ netlify/functions/challengeContentCheckAnswer.mjs
@@ -2,7 +2,7 @@ export default async (event, context) => {
   console.log("event log:");
   console.log(event);
   
-  const answer = event.url.searchParams.answer;
+  const answer = event.url.searchParams.get("answer");
   console.log(answer)
   
   return Response.json({ "isCorrect": (String(answer).toLowerCase() == String("Amazing job, very well done, your journey has just begun!").toLowerCase())})
