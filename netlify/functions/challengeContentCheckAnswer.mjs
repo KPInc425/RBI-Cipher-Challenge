3754a9f2168ffdfad9a612433fc9ae0972cbcadc
JamesK <James@10-geek.com>
2024-06-25 16:31:57 -0500

    Updating cipher contents

--- netlify/functions/challengeContentCheckAnswer.mjs
+++ netlify/functions/challengeContentCheckAnswer.mjs
@@ -1,7 +1,7 @@
 //import { createClient } from '@supabase/supabase-js'
 
 const challengeContentCheckAnswer = async ({ answerGuess }) => {
-  return Response.json({ "isCorrect": (answerGuess == "text 1")})
+  return Response.json({ "isCorrect": (answerGuess.toLower() == "Amazing job, very well done, your journey has just begun!".toLower())})
 }
 
 export default challengeContentCheckAnswer;
