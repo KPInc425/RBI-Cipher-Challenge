52590dee712ee92f4b2ca5c5b3bf228de549fdea
JamesK <James@10-geek.com>
2024-06-25 17:22:28 -0500

    logging

--- netlify/functions/challengeContentCheckAnswer.mjs
+++ netlify/functions/challengeContentCheckAnswer.mjs
@@ -1,6 +1,9 @@
 export default async (event, context) => {
-  console.log("event log:" + event);
+  console.log("event log:");
+  console.log(event);
   
   const answer = event.queryStringParameters;
+  console.log(answer)
+  
   return Response.json({ "isCorrect": (String(answer).toLowerCase() == String("Amazing job, very well done, your journey has just begun!").toLowerCase())})
 };
