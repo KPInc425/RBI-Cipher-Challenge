9fa3485a4c6d8918812005ac6666d66845d9a7bb
JamesK <James@10-geek.com>
2024-06-25 18:38:53 -0500

    toml

--- api/challengeContentCheckAnswer.mjs
+++ api/challengeContentCheckAnswer.mjs
@@ -2,3 +2,7 @@ export default async (event, context) => {
   const answer = new URL(event.url).searchParams.get("answer");
   return Response.json({ "isCorrect": (String(answer).toLowerCase() == String("Amazing job, very well done, your journey has just begun!").toLowerCase())})
 };
+
+export const config = {
+  path: "/api/challengeContentCheckAnswer"
+};
